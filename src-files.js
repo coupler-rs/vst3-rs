b9260146039f3f51c91e0a87c779d741cff68dd8
micahrj <micahrjohnston@gmail.com>
2025-11-26 15:51:26 +0000

    Deploying to gh-pages from @ coupler-rs/vst3-rs@f706b79e55373155022880c44a5d4ce2715f5e40 🚀

--- src-files.js
+++ src-files.js
@@ -1,2 +1,2 @@
-createSrcSidebar('[["com_scrape_types",["",[],["class.rs","lib.rs","ptr.rs"]]],["vst3",["",[],["lib.rs"]]]]');
-//{"start":19,"fragment_lengths":[59,28]}
+createSrcSidebar('[["com_scrape_types",["",[],["class.rs","lib.rs","ptr.rs"]]],["vst3",["",[],["lib.rs","support.rs"]]]]');
+//{"start":19,"fragment_lengths":[59,41]}
